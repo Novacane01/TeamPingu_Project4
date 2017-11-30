b7fb51b693c61eb22b0a0374e92d352dd656b9bf
Ankit <a.vishnubhotla@ufl.edu>
2017-11-29 22:08:35 -0500

    First Start

--- System/game/controllers/DefenderController.java
+++ System/game/controllers/DefenderController.java
@@ -1,12 +1,12 @@
 package game.controllers;
 
-import game.models.Game;
+		import game.models.Game;
 
 /*
  * Interface that Enemies controllers must implement. The only method that is
- * required is getActions(-), which returns the direction to be taken: 
+ * required is getActions(-), which returns the direction to be taken:
  * Up - Right - Down - Left -> 0 - 1 - 2 - 3
- * Any other number is considered to be a lack of action (Neutral). 
+ * Any other number is considered to be a lack of action (Neutral).
  */
 public interface DefenderController
 {
--- src/ufl/cs1/controllers/StudentController.java
+++ src/ufl/cs1/controllers/StudentController.java
@@ -1,13 +1,27 @@
 package ufl.cs1.controllers;
 
 import game.controllers.DefenderController;
-import game.models.Defender;
-import game.models.Game;
+import game.models.*;
 
 import java.util.List;
 
 public final class StudentController implements DefenderController
 {
+    public boolean isHungry(Game game, Attacker attacker) {
+        Maze bruh = game.getCurMaze();
+        List<Node> powerPills = bruh.getPowerPillNodes();
+
+        for (int i = 0; i < powerPills.size(); i++) {
+            double dist = Math.sqrt(Math.pow((attacker.getLocation().getX() - powerPills.get(i).getX()), 2) + Math.pow((attacker.getLocation().getY() - powerPills.get(i).getY()), 2));
+            if (dist <= 8) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public enum isHungry {EATEN, NOTEATEN, ONPILL};
+
 	public void init(Game game) { }
 
 	public void shutdown(Game game) { }
@@ -16,18 +30,38 @@ public final class StudentController implements DefenderController
 	{
 		int[] actions = new int[Game.NUM_DEFENDER];
 		List<Defender> enemies = game.getDefenders();
-		
-		//Chooses a random LEGAL action if required. Could be much simpler by simply returning
-		//any random number of all of the ghosts
+		Attacker pacman = game.getAttacker();
+
 		for(int i = 0; i < actions.length; i++)
 		{
 			Defender defender = enemies.get(i);
 			List<Integer> possibleDirs = defender.getPossibleDirs();
-			if (possibleDirs.size() != 0)
-				actions[i]=possibleDirs.get(Game.rng.nextInt(possibleDirs.size()));
+            int pacx = pacman.getLocation().getX();
+            int pacy = pacman.getLocation().getY();
+            int ghostx = defender.getLocation().getX();
+            int ghosty = defender.getLocation().getY();
+            int xdistance = Math.abs(pacx-ghostx);
+            int ydistance = Math.abs(pacy-ghosty);
+
+            if (possibleDirs.size() != 0) {
+                //actions[i]=possibleDirs.get(Game.rng.nextInt(possibleDirs.size()));
+                if(pacx>ghostx&&possibleDirs.contains(1)&&(xdistance>ydistance)) {
+                    actions[i] = 1;
+                }
+                if(pacx<ghostx&&possibleDirs.contains(3)&&(xdistance>ydistance)){
+                    actions[i] = 3;
+                }
+                if(pacy>ghosty&&possibleDirs.contains(2)&&(ydistance>xdistance)){
+                    actions[i] = 2;
+                }
+                if(pacy<ghosty&&possibleDirs.contains(0)&&(ydistance>xdistance)){
+                    actions[i] = 0;
+                }
+            }
 			else
 				actions[i] = -1;
 		}
+		System.out.println(actions[1]);
 		return actions;
 	}
 }
